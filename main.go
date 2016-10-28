13c89056b3159bdde7b7695cbb2163812ac6c37d
Mike Greenberg <mg@nobulb.com>
2016-10-28 17:41:45 -0400

    Extract ProxyHandler and implement my own routing within

--- main.go
+++ main.go
@@ -4,10 +4,45 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 var numberOfProxyWorkers = 5
 var workPool = make(chan bool, numberOfProxyWorkers)
+
+type ProxyHandler struct {
+	http.Handler
+	routes map[*regexp.Regexp]func(http.ResponseWriter, *http.Request)
+}
+
+func NewProxyHandler() *ProxyHandler {
+	p := &ProxyHandler{}
+	(*p).routes = (make(map[*regexp.Regexp]func(http.ResponseWriter, *http.Request)))
+	return p
+}
+
+func (h *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for regexp, h := range h.routes {
+		if regexp.Match(([]byte)(r.URL.Host + r.URL.Path)) {
+			h(w, r)
+		}
+	}
+}
+
+func (p *ProxyHandler) handleVia(request, destination string) {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		complete := make(chan bool)
+		workPool <- true
+		go proxyRequest(destination, w, complete)
+		<-complete
+	}
+	r, err := regexp.Compile(request)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	p.routes[r] = h
+
+}
 
 func copyHeaders(dst, src http.Header) {
 	for k := range src {
@@ -38,18 +73,9 @@ func proxyRequest(destination string, w http.ResponseWriter, complete chan<- boo
 	complete <- true
 }
 
-func handleVia(request, destination string) {
-	h := func(w http.ResponseWriter, r *http.Request) {
-		complete := make(chan bool)
-		workPool <- true
-		go proxyRequest(destination, w, complete)
-		<-complete
-	}
-	http.HandleFunc(request, h)
-}
-
 func main() {
-	handleVia("/", "http://google.com")
-	handleVia("/foo", "http://reddit.com")
-	log.Fatalln(http.ListenAndServe(":8080", nil))
+	p := NewProxyHandler()
+	p.handleVia("/", "http://google.com")
+	p.handleVia("/foo", "http://reddit.com")
+	log.Fatalln(http.ListenAndServe(":8080", p))
 }
